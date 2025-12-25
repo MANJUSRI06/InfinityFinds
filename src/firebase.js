3c4232b14b47151e596763893dbc6f863b60d567
Manjusri <2k23it27@kiot.ac.in>
2025-12-25 18:53:14 +0530

    Secure Firebase config using environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-    apiKey: "AIzaSyAdw0fLxCE78LGyGM87CH-DPGcTespnksk",
-    authDomain: "infinityfinds-cb740.firebaseapp.com",
-    projectId: "infinityfinds-cb740",
-    storageBucket: "infinityfinds-cb740.firebasestorage.app",
-    messagingSenderId: "931838511176",
-    appId: "1:931838511176:web:0e649bad2e6f51e08ef07d"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
